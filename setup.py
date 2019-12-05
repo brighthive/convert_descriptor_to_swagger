b3a675a208d8dda8feb11782cea650d6840a3573
Logan <logan@brighthive.io>
2019-12-04 16:40:47 -0800

    Inital commit.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='convert_descriptor_to_swagger',
+    version='0.1',
+    packages=['convert_descriptor_to_swagger'],
+    license='None',
+    long_description=open('README.md').read(),
+)
