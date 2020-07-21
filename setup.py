ac59504ddf70622fb76e68b23dfdf4555836d3bd
Logan <logan@brighthive.io>
2020-07-21 10:26:26 -0700

    fix: Declare deps.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name='convert_descriptor_to_swagger',
-    version='0.1',
-    packages=['convert_descriptor_to_swagger'],
-    license='None',
-    long_description=open('README.md').read(),
+    name="convert_descriptor_to_swagger",
+    version="0.1",
+    packages=["convert_descriptor_to_swagger"],
+    license="None",
+    long_description=open("README.md").read(),
+    install_requires=["deepmerge"],
 )
